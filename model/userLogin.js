e46b7e9ad22e6887b89807bd2019939c0e30ef3b
Jesse <1956125@cegep-heritage.qc.ca>
2022-03-21 11:55:54 -0400

    testing getUser function

--- model/userLogin.js
+++ model/userLogin.js
@@ -5,7 +5,7 @@ let users = {};
 users.getUser = async(user) => {
     let dbConn = await dbConnPool.getConnection();
     const rows = await dbConn.query("SELECT * FROM `users` WHERE userName = ?;", [user]);
-    users.push(rows);
+
     console.log(users);
     dbConn.end();
 }
